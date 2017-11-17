ca150908466509d43beb4b22bd0dd615a110314f
Juan Ibiapina <juanibiapina@gmail.com>
2017-11-17 13:52:44 +0100

    feature: Blank screen, quit with 'q'
    Small experiment on using ncurses.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+extern crate ncurses;
+
+use ncurses::*;
+
+fn init() {
+    initscr();
+    keypad(stdscr(), true);
+    noecho();
+}
+
+fn finish() {
+    endwin();
+}
+
 fn main() {
-    println!("Hello, world!");
+    init();
+
+    loop {
+        let ch = getch();
+        match ch {
+            113 => break,
+            _ => continue,
+        }
+    }
+
+    finish();
 }
